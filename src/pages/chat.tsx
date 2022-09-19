8e74a0bb10035f93e5ebc8683f296b2463a6b2c9
vu <duyvujarvan4@gmail.com>
2022-09-19 15:20:15 +0700

    update

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,13 +1,11 @@
-import { Chat, Header } from "@/components"
+import { Header } from "@/components"
 
 const ChatPage = () => {
   return (
     <>
       <Header />
       <section className="bg-bg flex flex-col min-h-[calc(100vh-80px)] py-24 chat-page">
-        <div className="container flex-1 flex flex-col">
-          {/* <Chat /> */}
-        </div>
+        <div className="container flex-1 flex flex-col">{/* <Chat /> */}</div>
       </section>
     </>
   )
